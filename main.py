f0c1783277a88d7859db051bf714b54b1a5eaa18
NRand-Dev <nrand_dev@protonmail.com>
2026-09-02 15:17:17 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from tkinter import constants
+
+import pygame
+from pygame.color import Color
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    # Initalize the game
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # While pygame is running, call log state.
+    while pygame.get_init() == True:
+        log_state()
+        # Process pygame event queue.
+        for event in pygame.event.get():
+            # Quit Button Handling
+            if event.type == pygame.QUIT:
+                return
+
+        # Draw the screen
+        screen.fill("black")
+        # Refresh the screen
+        pygame.display.flip()
+
+
+
+## Keep this here. This line ensures that the main function is only called when this file is run directly.
+# Would not be ran if imported.
+# Considered proper structure for python.
+if __name__ == "__main__":
+    main()
